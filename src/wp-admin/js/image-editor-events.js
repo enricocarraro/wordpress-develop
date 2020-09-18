f008d93ce47a5fc1232b52f2622a47d69519eddc
Enrico Carraro <enricocarraro@google.com>
2020-09-18 10:48:47 +0200

    Move inline event handlers image edit

--- src/wp-admin/js/image-editor-events.js
+++ src/wp-admin/js/image-editor-events.js
@@ -11,87 +11,112 @@
  *
  * @return {void}
  */
-(function (document, window) {
-    document.addEventListener('DOMContentLoaded', function () {
-        jQuery("div.wp_attachment_holder").on("load", "imgedit-panel-content", function () {
-            const item = document.querySelector(
-                "img.image-preview"
-            );
-            item.addEventListener('load', function() {
-                imageEdit.imgLoaded(item.dataset.postId);
-            });
-            
-        });
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-crop", function () {
-            const item = document.querySelector(
-                ".imgedit-crop"
-            );
-            imageEdit.handleCropToolClick(parseInt(item.dataset.postId),
-                item.dataset.nonce, item);
-        });
+( function ( document, window ) {
+	document.addEventListener( 'DOMContentLoaded', function () {
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'load',
+			'img.image-preview',
+			function () {
+				const item = document.querySelector( 'img.image-preview' );
+				imageEdit.imgLoaded( item.dataset.postId );
+			}
+		);
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-crop',
+			function () {
+				const item = document.querySelector( '.imgedit-crop' );
+				imageEdit.handleCropToolClick(
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-rleft", function () {
-            const item = document.querySelector(
-                ".imgedit-rleft"
-            );
-            imageEdit.rotate(
-                90,
-                parseInt(item.dataset.postId),
-                item.dataset.nonce,
-                item);
-        });
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-rleft',
+			function () {
+				const item = document.querySelector( '.imgedit-rleft' );
+				imageEdit.rotate(
+					90,
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-rright", function () {
-            const item = document.querySelector(
-                ".imgedit-rright"
-            );
-            imageEdit.rotate(
-                -90,
-                parseInt(item.dataset.postId),
-                item.dataset.nonce,
-                item);
-        });
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-rright',
+			function () {
+				const item = document.querySelector( '.imgedit-rright' );
+				imageEdit.rotate(
+					-90,
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-flipv", function () {
-            const item = document.querySelector(
-                ".imgedit-flipv"
-            );
-            imageEdit.flip(1, parseInt(item.dataset.postId),
-                item.dataset.nonce,
-                item);
-        });
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-flipv',
+			function () {
+				const item = document.querySelector( '.imgedit-flipv' );
+				imageEdit.flip(
+					1,
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-fliph", function () {
-            const item = document.querySelector(
-                ".imgedit-fliph"
-            );
-            imageEdit.flip(2, parseInt(item.dataset.postId),
-                item.dataset.nonce,
-                item);
-        });
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-fliph',
+			function () {
+				const item = document.querySelector( '.imgedit-fliph' );
+				imageEdit.flip(
+					2,
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-undo", function () {
-            const item = document.querySelector(
-                ".imgedit-undo"
-            );
-            imageEdit.undo(parseInt(item.dataset.postId),
-                item.dataset.nonce,
-                item);
-        });
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-undo',
+			function () {
+				const item = document.querySelector( '.imgedit-undo' );
+				imageEdit.undo(
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        jQuery("div.wp_attachment_holder").on("click", ".imgedit-redo", function () {
-            const item = document.querySelector(
-                ".imgedit-redo"
-            );
-            imageEdit.redo(parseInt(item.dataset.postId),
-                item.dataset.nonce,
-                item);
-        });
+		jQuery( 'div.wp_attachment_holder' ).on(
+			'click',
+			'.imgedit-redo',
+			function () {
+				const item = document.querySelector( '.imgedit-redo' );
+				imageEdit.redo(
+					parseInt( item.dataset.postId ),
+					item.dataset.nonce,
+					item
+				);
+			}
+		);
 
-        
-
-
-        /*   const cancelAsyncUpload = document.getElementById(
+		/*   const cancelAsyncUpload = document.getElementById(
                'cancel-async-upload'
            );
            if (cancelAsyncUpload !== null) {
@@ -102,7 +127,5 @@
                    event.preventDefault();
                });
            }*/
-
-
-    });
-})(document, window);
+	} );
+} )( document, window );
