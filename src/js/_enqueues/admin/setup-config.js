7ae1e2dfb73e264d725aab3503a6a7f3c695bc17
Enrico Carraro <enricocarraro@google.com>
2020-09-22 11:52:12 +0200

    Moved back inline script (setup-config.php)

--- src/js/_enqueues/admin/setup-config.js
+++ src/js/_enqueues/admin/setup-config.js
@@ -2,28 +2,17 @@
  * @output wp-admin/js/setup-config.js
  */
 /**
- * Setup config scripts
+ * Setup config event hanlder
  *
  * @since 5.6.0
  *
  * @param {Object} document  The document object.
  * @param {Object} window    The window object.
- * @param {Object} navigator The navigator object.
  *
  * @return {void}
  */
-( function ( document, window, navigator ) {
+( function ( document, window ) {
 	document.addEventListener( 'DOMContentLoaded', function () {
-		if ( ! /iPad|iPod|iPhone/.test( navigator.userAgent ) ) {
-			var wpConfigTextArea = document.querySelector(
-				'#wp-config.not-writable'
-			);
-			if ( wpConfigTextArea !== null ) {
-				wpConfigTextArea.focus();
-				wpConfigTextArea.select();
-			}
-		}
-
 		var tryAgain = document.getElementById( 'try-again-link' );
 		if ( tryAgain !== null ) {
 			tryAgain.addEventListener( 'click', function ( event ) {
@@ -32,4 +21,4 @@
 			} );
 		}
 	} );
-} )( document, window, navigator );
+} )( document, window );
