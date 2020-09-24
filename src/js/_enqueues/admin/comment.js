b9ba86cf61ef4de6caf7a705d4fd9ba2c2b7f445
Enrico Carraro <enricocarraro@google.com>
2020-09-24 17:32:06 +0200

    Add description of event handler

--- src/js/_enqueues/admin/comment.js
+++ src/js/_enqueues/admin/comment.js
@@ -100,6 +100,7 @@ jQuery(document).ready( function($) {
 		$timestampdiv.slideUp( 'fast' );
 	});
 
+	// Event handler for "Go back" link in case of invalid comment ID.
 	$('#go-back').click( function () {
 		window.history.go(-1);
 		return false;
